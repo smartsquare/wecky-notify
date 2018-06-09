7d25c11feb0599eaf063d77f761ea1bafb02dadb
deen13 <dennis.dierkes@icloud.com>
2018-06-09 09:32:44 +0200

    Project setup

---
+++ src/test/kotlin/de/smartsquare/wecky/ApplicationTests.kt
@@ -0,0 +1,15 @@
+package de.smartsquare.wecky
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@SpringBootTest
+@RunWith(SpringRunner::class)
+class DemoApplicationTests {
+
+    @Test
+    fun contextLoads() {}
+
+}
